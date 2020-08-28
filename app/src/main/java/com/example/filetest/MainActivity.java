1ec6e882251c7dbfaf770fe20529985696c016f7
Alireza <alireza1020003@gmail.com>
2020-08-28 19:02:40 +0430

    view initialized updated(buttons)

--- app/src/main/java/com/example/filetest/MainActivity.java
+++ app/src/main/java/com/example/filetest/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     EditText inpt_name,inpt_content;
+    Button btn_save,btn_load;
     TextView text;
 
     @Override
@@ -28,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         inpt_name=findViewById(R.id.inpt_name_file);
         inpt_content=findViewById(R.id.inpt_content_file);
         text=findViewById(R.id.text);
+        btn_save=findViewById(R.id.btn_save);
+        btn_load=findViewById(R.id.btn_load);
     }
 
     public void save_it(View view) {
